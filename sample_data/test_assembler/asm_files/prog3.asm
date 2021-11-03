22cf0f115ef4b14f836c65f937da0a1c10c905df
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2021-11-03 14:57:16 +0530

    file changes

---
+++ sample_data/test_assembler/asm_files/prog3.asm
@@ -0,0 +1,7 @@
+and $s1,$s2,$s3
+or $s1,$s2,$s3
+nor $s1,$s2,$s3
+andi $s1,$s2,20
+ori $s1,$s2,20
+sll $s1,$s2,10
+srl $s1,$s2,10
---
+++ sample_data/test_assembler/asm_files/prog4.asm
@@ -0,0 +1,6 @@
+beq $s1,$s2,25
+bne $s1,$s2,25
+slt $s1,$s2,$s3
+sltu $s1,$s2,$s3
+slti $s1,$s2,20
+sltiu $s1,$s2,20
